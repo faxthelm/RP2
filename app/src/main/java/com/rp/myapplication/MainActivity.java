ae9f4dfeba0baf0d1ac99da20d6e820e42ea1b13
AnaCarbone <anabcarbone@gmail.com>
2018-10-30 12:04:11 -0300

    Criação do menu

--- app/src/main/java/com/rp/myapplication/MainActivity.java
+++ app/src/main/java/com/rp/myapplication/MainActivity.java
@@ -6,6 +6,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -33,6 +36,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
         else if(view.getId() == R.id.helpButton){
             ((ManageEmergencyContactsActivity)mContext).sendSms();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.home:
+                return true;
+            case R.id.ong:
+                Intent intent = new Intent(this, ONGInformation.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 }
